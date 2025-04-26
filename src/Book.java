dc38e8c472cccda7472389d4f1123b66ec9b608c
safouane-naceur <168561443+safouane-naceur@users.noreply.github.com>
2025-04-26 18:44:24 +0200

    Update and rename Book to Book.java

---
+++ src/Book.java
@@ -0,0 +1,23 @@
+class Book {
+    private String title;
+    private String author;
+    private int ISBN;
+    private boolean isBorrowed;
+
+    public Book(String title, String author, int ISBN) {
+        this.title = title;
+        this.author = author;
+        this.ISBN = ISBN;
+        this.isBorrowed = false;
+    }
+
+    // Getters
+    public String getTitle() { return title; }
+    public String getAuthor() { return author; }
+    public int getISBN() { return ISBN; }
+    public boolean isBorrowed() { return isBorrowed; }
+
+    // Setters
+    public void setBorrowed(boolean borrowed) { isBorrowed = true; }
+}
+
