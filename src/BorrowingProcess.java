d8d724e4a918ad26c9a4235433af1572b4dab228
safouane-naceur <168561443+safouane-naceur@users.noreply.github.com>
2025-04-27 18:05:43 +0200

    Update BorrowingProcess.java

--- src/BorrowingProcess.java
+++ src/BorrowingProcess.java
@@ -1,32 +1,35 @@
 import java.util.Date;
 
 public class BorrowingProcess {
-    private Book borrowedBook;
-    private Borrower borrower;
-    private Date borrowDate;
-    private Date returnDate;
-
-    public BorrowingProcess(Book borrowedBook, Borrower borrower) {
-        this.borrowedBook = borrowedBook;
-        this.borrower = borrower;
-        this.borrowDate = new Date(); // Current date
-        this.returnDate = null; // Will be set when book is returned
-    }
-
-    // Getters
-    public Book getBorrowedBook() { return borrowedBook; }
-    public Borrower getBorrower() { return borrower; }
-    public Date getBorrowDate() { return borrowDate; }
-    public Date getReturnDate() { return returnDate; }
-
-    // Method to return the book
-    public void returnBook() {
-        this.returnDate = new Date();
-        this.borrowedBook.setBorrowed(false);
-    }
-
-    // Method to check if book is returned
-    public boolean isReturned() {
-        return returnDate != null;
-    }
-} 
+     private Book borrowedBook;
+     private Borrower borrower;
+     private Date borrowDate;
+     private Date returnDate;
+ 
+     // Creates a new borrowing process record when a book is borrowed
+  
+     public BorrowingProcess(Book borrowedBook, Borrower borrower) {
+         this.borrowedBook = borrowedBook;
+         this.borrower = borrower;
+         this.borrowDate = new Date(); // Current date
+         this.returnDate = null; // Will be set when book is returned
+     }
+ 
+     // Getters
+     public Book getBorrowedBook() { return borrowedBook; }
+     public Borrower getBorrower() { return borrower; }
+     public Date getBorrowDate() { return borrowDate; }
+     public Date getReturnDate() { return returnDate; }
+     
+     // Records the return of the book
+   
+     public void recordReturn() {
+         this.returnDate = new Date();
+     }
+ 
+     
+     public boolean isReturned() {
+         return returnDate != null;
+     }
+ 
+ }
